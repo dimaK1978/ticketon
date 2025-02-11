6e68b0324763bc3b8eccfb1924b31aeabb39f459
”Alex <masson80.05@gmail.com>
2025-02-11 10:07:45 +0500

    рабочая версия проекта

---
+++ finalsurge/src/test/java/utils/SleepUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.util.concurrent.TimeUnit;
+
+public class SleepUtils {
+    static public void sleepSeconds(final long seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
